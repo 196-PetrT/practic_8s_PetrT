061e3e5418b828a1cbb31c340713f66b82f675a1
PetrT <140140070+196-PetrT@users.noreply.github.com>
2023-09-11 20:51:37 +0300

    sort iten in rows from max to min

---
+++ task54/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] GetMatrix(int rows, int columns, int minValue = 1, int maxValue = 10)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = rand.Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix) // метод вывода массива на экран
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SortMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int minPosition = 0; minPosition < matrix.GetLength(1) - 1; minPosition++)
+            if (matrix[i, minPosition] < matrix[i, minPosition + 1])
+            {
+                int temp = matrix[i, minPosition + 1];
+                matrix[i, minPosition + 1] = matrix[i, minPosition];
+                matrix[i, minPosition] = temp;
+            }
+        }
+    }
+    Console.WriteLine();
+}
+
+int[,] matrix = GetMatrix(3, 4);
+PrintMatrix(matrix);
+SortMatrix(matrix);
+PrintMatrix(matrix);
